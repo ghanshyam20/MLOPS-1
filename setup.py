890cbf5ac08b52381e6f8c00bf31127a0beb8cb7
ghanshyam20 <bhattaraighanshyam274@gmail.com>
2025-07-17 13:59:02 +0300

    data versioning

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup,find_packages
+
+with open("requirements.txt") as f:
+    requirements=f.read().splitlines()
+
+setup(
+    name="MLOPS-PROJECT-1",
+    version="0.1",
+    author="ghanshyam",
+    author_email="bhattaraighanshyam274@gmail.com",
+    packages=find_packages(),
+    install_requires=requirements,
+)
+
+
+
